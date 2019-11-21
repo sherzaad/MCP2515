0e9be3bf03951c0757c0c84c5dedad2f9c1dd17a
sherzaad <57634578+sherzaad@users.noreply.github.com>
2019-11-21 12:13:08 +0000

    Add files via upload

---
+++ mcp2515.cpp
@@ -0,0 +1,454 @@
+/*
+mcp2515.ccp - Library for mcp2015 Arduino CAN shield
+ver1.0 created by Sherzaad Dinah
+*/
+
+#include <mcp2515.h>
+
+uint8_t MCP2515::get_message(tCAN *message)
+{
+uint8_t r_status, addr, length;
+  // read status
+  r_status = read_status(SPI_RX_STATUS);
+
+  //SPI.begin();
+  
+  if (BIT_CHK(r_status,6)) {
+    // message in buffer 0
+    addr = SPI_READ_RX;
+  }
+  else if (BIT_CHK(r_status,7)) {
+    // message in buffer 1
+    addr = SPI_READ_RX | 0x04;
+  }
+  else {
+    // Error: no message available
+    return 0;
+  }
+
+  RESET_CS();
+  SPI.transfer(addr);
+  
+  message->header.rtr = BIT_CHK(r_status, 3);
+  message->header.ide = BIT_CHK(r_status, 4); 
+  
+  // read ex-id
+  if(message->header.ide){
+	message->id.stdh  = (uint16_t) SPI.transfer(0xff) << 5;
+	uint8_t temp = SPI.transfer(0xff);
+	message->id.stdh |= (((temp >> 3)&0xFC)|(temp&0x03));
+	message->id.std = (uint16_t) SPI.transfer(0xff) << 8;
+	message->id.std |= SPI.transfer(0xff);
+  }
+  else{
+	// read std-id
+	message->id.std  = (uint16_t) SPI.transfer(0xff) << 3;
+	message->id.std |=            SPI.transfer(0xff) >> 5;
+	SPI.transfer(0xff);
+	SPI.transfer(0xff);
+  }
+  
+  // read DLC
+  length = SPI.transfer(0xff) & 0x0f;
+  
+  message->header.dlc = length;
+  
+  // read data
+  for (uint8_t t=0;t<length;t++) {
+    message->data[t] = SPI.transfer(0xff);
+  }
+  SET_CS();
+  
+  // clear interrupt flag
+  if (BIT_CHK(r_status, 6)) {
+    bit_modify(CANINTF, (1<<RX0IF),0);
+  }
+  else {
+    bit_modify(CANINTF, (1<<RX1IF), 0);
+  }
+  
+  return (r_status & 0x07) + 1;
+}
+
+uint8_t MCP2515::send_message(tCAN *message, uint8_t *alt_data[ ]=nullptr)
+{
+uint8_t r_status, addr, length;
+
+  r_status = read_status(SPI_READ_STATUS);
+  
+  /* Statusbyte:
+   *
+   * Bit  Function
+   *  2 TXB0CNTRL.TXREQ
+   *  4 TXB1CNTRL.TXREQ
+   *  6 TXB2CNTRL.TXREQ
+   */
+
+  //SPI.begin();
+  
+  if (!(BIT_CHK(r_status, 2))) {
+    addr = 0x00;
+  }
+  else if (!(BIT_CHK(r_status, 4))) {
+    addr = 0x02;
+  } 
+  else if (!(BIT_CHK(r_status, 6))) {
+    addr = 0x04;
+  }
+  else {
+    // all buffer used => could not send message
+    return 0;
+  }
+  
+  RESET_CS();
+  SPI.transfer(SPI_WRITE_TX | addr);
+
+
+  //send ex-id
+  if(message->header.ide){
+	SPI.transfer(message->id.stdh >> 5);
+	uint8_t temp = ((message->id.stdh << 3)&0xE0)|((message->id.stdh)&0x03)|0x08; //)0x08 : EXIDE=1
+	SPI.transfer(temp);
+	SPI.transfer(message->id.std >> 8);
+	SPI.transfer(message->id.std);
+  }
+  else{
+	// send std-id
+	  SPI.transfer(message->id.std >> 3);
+	  SPI.transfer(message->id.std << 5);
+	  SPI.transfer(0);
+	  SPI.transfer(0);
+  }  
+
+  
+  length = message->header.dlc & 0x0f;
+  
+  if (message->header.rtr) {
+    // a rtr-frame has a length, but contains no data
+    SPI.transfer((1<<RTR) | length);
+  }
+  else {
+    // set message length
+    SPI.transfer(length);
+    
+    // data
+	if(alt_data==nullptr){
+		for (uint8_t t=0;t<length;t++) {
+			SPI.transfer(message->data[t]);
+		}
+	}
+	else{
+		for (uint8_t t=0;t<length;t++) {
+			SPI.transfer(*alt_data[t]);
+		}	
+	}
+  }
+  SET_CS();
+  
+  //_delay_us(1);
+  __asm__("nop\n\t""nop\n\t""nop\n\t""nop\n\t"); //nanosecond delay (4 * 1/freq clk)
+												 //small delay to allow CS switching during SET/RESET
+  // send message
+  RESET_CS();
+  addr = (addr == 0) ? 1 : addr;
+  SPI.transfer(SPI_RTS | addr);
+  SET_CS();
+  
+  return addr;
+}
+
+uint8_t MCP2515::CAN_init(uint8_t cnf1, uint8_t cnf2, uint8_t cnf3)
+{ 
+	//initialise IOs and SPI comms
+	pinMode(cs_pin,OUTPUT);   //MCP2515_CS
+	digitalWrite(cs_pin,HIGH);//Deselect Chip
+	pinMode(intr_pin,INPUT);     //MCP2515_INT
+	
+	#ifndef SPI_Init
+		#define SPI_Init 1
+		//SPI clock speed:10MHz, Data Shift:MSB First, Data Clock Idle: SPI_MODE0
+		SPI.beginTransaction(SPISettings(10000000,MSBFIRST,SPI_MODE0)); 
+		SPI.begin();
+	#endif
+  
+  // reset MCP2515 by software reset.
+  // After this he is in configuration mode.
+  RESET_CS();
+  SPI.transfer(SPI_RESET);
+  SET_CS();
+  
+  //1ms delay to wait until the MCP2515 has restarted
+  delay(1);
+  
+  // load CNF1..3 Register
+  RESET_CS();
+  SPI.transfer(SPI_WRITE);
+  SPI.transfer(CNF3);
+ 
+  SPI.transfer(cnf3);   // Bitrate 250 kbps at 16 MHz
+  SPI.transfer(cnf2);
+
+  SPI.transfer(cnf1);
+
+  // activate interrupts
+  SPI.transfer((1<<RX1IE)|(1<<RX0IE));
+  SET_CS();
+  
+  // test if we could read back the value => is the chip accessible?
+  if (read_register(CNF1) != cnf1) {
+    return 0;
+  }
+  
+  // deaktivate the RXnBF Pins (High Impedance State)
+  write_register(BFPCTRL, 0);
+  
+  // set TXnRTS as inputs
+  write_register(TXRTSCTRL, 0);
+  
+  // turn off filters => receive any message
+  write_register(RXB0CTRL, (1<<RXM1)|(1<<RXM0));
+  write_register(RXB1CTRL, (1<<RXM1)|(1<<RXM0));
+  
+  // reset device to MODE_LISTENONLY
+  write_register(CANCTRL, MODE_LISTENONLY);
+  
+  return 1;
+}
+
+uint8_t MCP2515::set_filter_mask(uint8_t mask_reg, uint16_t mask)
+{
+  // config mode
+  if (read_register(CANCTRL) != MODE_CONFIG){
+	write_register(CANCTRL, MODE_CONFIG);
+	if (read_register(CANCTRL) != MODE_CONFIG) return 0;
+  }
+  
+  uint8_t data[4] = {(mask >> 3),(mask << 5),0,0};
+  
+  write_register(mask_reg, data, 4); 
+  
+  #ifdef HW_Mode
+	//reset device initial set mode
+	write_register(CANCTRL, mode_chip);
+  #else
+    //reset device to MODE_LISTENONLY
+    write_register(CANCTRL, MODE_LISTENONLY);
+  #endif
+  
+  return 1;
+}
+
+uint8_t MCP2515::set_filter_mask(uint8_t mask_reg, uint32_t mask)
+{
+union { 
+	uint32_t ex;
+	struct{
+		uint16_t std;
+		uint16_t stdh;
+	};
+} id;
+
+  // config mode
+  if (read_register(CANCTRL) != MODE_CONFIG){
+	write_register(CANCTRL, MODE_CONFIG);
+	if (read_register(CANCTRL) != MODE_CONFIG) return 0;
+  }
+
+  id.ex = mask;
+  
+  uint8_t data[4] = {(id.stdh >> 5),((id.stdh << 3)&0xE0)|(id.stdh&0x03),(id.std >> 8),id.std};
+  
+  write_register(mask_reg, data, 4); 
+  
+  #ifdef HW_Mode
+	//reset device initial set mode
+	write_register(CANCTRL, mode_chip);
+  #else
+    //reset device to MODE_LISTENONLY
+    write_register(CANCTRL, MODE_LISTENONLY);
+  #endif
+  
+  return 1;
+}
+
+uint8_t MCP2515::enable_filter()
+{
+uint8_t rxbnctrl;
+
+  // config mode
+  if (read_register(CANCTRL) != MODE_CONFIG){
+	write_register(CANCTRL, MODE_CONFIG);
+	if (read_register(CANCTRL) != MODE_CONFIG) return 0;
+  }
+  
+  
+  // turn on filters => Receive all valid messages using either standard or extended identifiers that meet filter criteria
+  rxbnctrl = (~((1<<RXM1)|(1<<RXM0))) & read_register(RXB0CTRL);
+	
+  write_register(RXB0CTRL,rxbnctrl);
+	
+  rxbnctrl = (~((1<<RXM1)|(1<<RXM0))) & read_register(RXB1CTRL);
+	
+  write_register(RXB1CTRL,rxbnctrl);
+	
+  #ifdef HW_Mode
+	//reset device initial set mode
+	write_register(CANCTRL, mode_chip);
+  #else
+    //reset device to MODE_LISTENONLY
+    write_register(CANCTRL, MODE_LISTENONLY);
+  #endif
+  
+  return 1;
+}
+
+uint8_t MCP2515::disable_filter()
+{
+  // config mode
+  if (read_register(CANCTRL) != MODE_CONFIG){
+	write_register(CANCTRL, MODE_CONFIG);
+	if (read_register(CANCTRL) != MODE_CONFIG) return 0;
+  }
+  
+  // turn off filters => receive any message
+  write_register(RXB0CTRL, (1<<RXM1)|(1<<RXM0));
+  write_register(RXB1CTRL, (1<<RXM1)|(1<<RXM0));
+  
+  // reset device to normal mode
+  write_register(CANCTRL, 0);
+  
+  return 1;
+}
+
+uint8_t MCP2515::init_filter(uint16_t *Std_CAN_id_Arr, uint8_t std_arr_size, uint32_t *Ex_CAN_id_Arr, uint8_t ex_arr_size)
+{
+uint8_t val;
+
+	if(std_arr_size+ex_arr_size>6) return 0;
+	
+	if(std_arr_size>2){
+		val = init_filter(Std_CAN_id_Arr, std_arr_size, 4); //use 2nd mask filter for std msg 
+		val &= init_filter(Ex_CAN_id_Arr, ex_arr_size, 1);	 //use 1st mask filter for ext msg
+	}
+	else{
+		val = init_filter(Std_CAN_id_Arr, std_arr_size, 1); //use 1st mask filter for std msg
+		val &= init_filter(Ex_CAN_id_Arr, ex_arr_size, 4);	 //use 2nd mask filter for ext msg	
+	}
+	
+	return val;
+}
+
+uint8_t MCP2515::init_filter(uint16_t *Std_CAN_id_Arr, uint8_t std_arr_size, uint8_t reg_select)
+{
+uint8_t filter_regs[8] = {RXM0SIDH, RXF0SIDH, RXF1SIDH, RXM1SIDH, RXF2SIDH, RXF3SIDH, RXF4SIDH, RXF5SIDH};
+uint8_t u_bound, rxbnctrl, i=0, j=0;
+
+	// config mode
+    write_register(CANCTRL, MODE_CONFIG);
+	
+    if (read_register(CANCTRL) != MODE_CONFIG) return 0;
+	else if(std_arr_size>6) return 0;
+	else{
+		uint8_t data[4] = {0xFF,0xFF,0xFF,0xFF};
+		
+		//init RX mask registers. 11-bit mask: 0x7FF, 29-bit mask: 0x1FFFFFFF <----No masking applied to filter CAN Ids	
+		write_register(filter_regs[((reg_select==1) ? 3: 0)], data, 4);
+		
+		if(reg_select>0){
+			data[2]=0x00;
+			data[3]=0x00;
+		}
+		
+		write_register(filter_regs[((reg_select==1) ? 0: 3)], data, 4);
+	}
+	
+	u_bound = ((reg_select==1) ? 3: 8);
+	
+	for(i=reg_select;i<u_bound;++i){
+		uint16_t std = *(Std_CAN_id_Arr+((j<std_arr_size) ? j: 0));
+		uint8_t data[4] = {(std >> 3),(std<< 5),0,0};
+		write_register(filter_regs[i], data, 4);
+		++j;	
+	}
+	
+	// turn on filters => Receive all valid messages using either standard or extended identifiers that meet filter criteria
+	rxbnctrl = ((~((1<<RXM1)|(1<<RXM0))) & read_register(RXB0CTRL))|(1<<BUKT);
+	
+	write_register(RXB0CTRL,rxbnctrl);
+	
+	rxbnctrl = (~((1<<RXM1)|(1<<RXM0))) & read_register(RXB1CTRL);
+	
+	write_register(RXB1CTRL,rxbnctrl);
+	
+  #ifdef HW_Mode
+	//reset device initial set mode
+	write_register(CANCTRL, mode_chip);
+  #else
+    //reset device to MODE_LISTENONLY
+    write_register(CANCTRL, MODE_LISTENONLY);
+  #endif
+	
+	return 1;
+}
+
+uint8_t MCP2515::init_filter(uint32_t *Ex_CAN_id_Arr, uint8_t ex_arr_size, uint8_t reg_select)
+{
+union { 
+	uint32_t ex;
+	struct{
+		uint16_t std;
+		uint16_t stdh;
+	};
+} id;
+
+uint8_t filter_regs[8] = {RXM0SIDH, RXF0SIDH, RXF1SIDH, RXM1SIDH, RXF2SIDH, RXF3SIDH, RXF4SIDH, RXF5SIDH};
+uint8_t u_bound, rxbnctrl, i=0, k=0;
+
+	// config mode
+    write_register(CANCTRL, MODE_CONFIG);
+	
+    if (read_register(CANCTRL) != MODE_CONFIG) return 0;
+	else if(ex_arr_size>6) return 0;
+	else{
+		uint8_t data[4] = {0xFF,0xFF,0xFF,0xFF};
+		
+		//init RX mask registers. 11-bit mask: 0x7FF, 29-bit mask: 0x1FFFFFFF <----No masking applied to filter CAN Ids	
+		write_register(filter_regs[((reg_select==1) ? 0: 3)], data, 4);
+		
+		if(reg_select>0){
+			data[2]=0x00;
+			data[3]=0x00;
+		}
+		
+		write_register(filter_regs[((reg_select==1) ? 3: 0)], data, 4);
+	}
+	
+	u_bound = ((reg_select==1) ? 3: 8);
+	
+	for(i=reg_select;i<u_bound;++i){
+		id.ex = *(Ex_CAN_id_Arr+((k<ex_arr_size) ? k: 0));
+		uint8_t data[4] = {(id.stdh >> 5),(((id.stdh << 3)&0xE0)|(id.stdh&0x03)|0x08),(id.std >> 8),id.std};
+		write_register(filter_regs[i], data, 4);			
+		++k;		
+	}
+
+	// turn on filters => Receive all valid messages using either standard or extended identifiers that meet filter criteria
+	rxbnctrl = ((~((1<<RXM1)|(1<<RXM0))) & read_register(RXB0CTRL))|(1<<BUKT);
+	
+	write_register(RXB0CTRL,rxbnctrl);
+	
+	rxbnctrl = (~((1<<RXM1)|(1<<RXM0))) & read_register(RXB1CTRL);
+	
+	write_register(RXB1CTRL,rxbnctrl);
+	
+  #ifdef HW_Mode
+	//reset device initial set mode
+	write_register(CANCTRL, mode_chip);
+  #else
+    //reset device to MODE_LISTENONLY
+    write_register(CANCTRL, MODE_LISTENONLY);
+  #endif
+	
+	return 1;
+}	
+
---
+++ mcp2515.h
@@ -0,0 +1,204 @@
+/*
+mcp2515.h - Library for mcp2015 Arduino CAN shield
+ver1.1 created by Sherzaad Dinah
+
+Revision History
+ver1.0 - Newly created
+ver1.1 - added tCAN modify_message(tCAN *message, uint16_t byte_arr[]) inline function. Allows bytes within received CAN message to be modified
+*/
+#ifndef	MCP2515_H
+#define	MCP2515_H
+
+#include <Arduino.h>
+#include <mcp2515_defs.h>
+#include <SPI.h>
+
+#define BIT_CHK(x,y) (((x)&(1<<(y)))>0? 1:0) //returns value selected bit '0' or '1'
+
+typedef struct
+{
+	union { 
+	    uint32_t ex;
+		struct{
+			uint16_t std;
+			uint16_t stdh;
+		};
+    } id;
+	struct {
+		int8_t rtr : 1;
+		uint8_t ide : 1; //IDE: IDentifier Extension bit - is '1' for extended frame format with 29-bit identifiers 		
+		uint8_t dlc : 4;
+	} header;
+	uint8_t data[8];
+} tCAN;
+
+class MCP2515{
+
+public:
+	uint8_t get_message(tCAN *message);
+	
+	uint8_t send_message(tCAN *message, uint8_t *alt_data[ ]=nullptr);
+	
+	uint8_t CAN_init(uint8_t cnf1, uint8_t cnf2 = (1<<BTLMODE)|(1<<PHSEG11), uint8_t cnf3 = (1<<PHSEG21));
+	
+	uint8_t init_filter(uint16_t *Std_CAN_id_Arr, uint8_t std_arr_size, uint32_t *Ex_CAN_id_Arr, uint8_t ex_arr_size);
+	
+	uint8_t init_filter(uint16_t *Std_CAN_id_Arr, uint8_t std_arr_size, uint8_t reg_select=0);
+	
+	uint8_t init_filter(uint32_t *Ex_CAN_id_Arr, uint8_t ex_arr_size, uint8_t reg_select=0);
+	
+	uint8_t enable_filter();
+	
+	uint8_t disable_filter();
+	
+	uint8_t set_filter_mask(uint8_t mask_reg, uint32_t mask);
+	
+	uint8_t set_filter_mask(uint8_t mask_reg, uint16_t mask);
+
+	inline void set_mode(uint8_t mode)
+	{
+		/*five modes of operation available
+			1. Configuration mode (MODE_CONFIG)
+			2. Normal mode (MODE_NORMAL)
+			3. Sleep mode (MODE_SLEEP)
+			4. Listen-Only mode (MODE_LISTENONLY)
+			5. Loopback mode (MODE_LOOPBACK)
+		*/
+		
+		
+		#ifdef HW_Mode
+			#undef HW_Mode
+		#endif
+		
+		#define HW_Mode
+		mode_chip = mode;
+		
+		bit_modify(CANCTRL, (1 << REQOP2) | (1 << REQOP1) | (1 << REQOP0), mode_chip);
+		
+	}
+	
+	inline void RESET_CS()
+	{
+		digitalWrite(cs_pin,LOW);
+	}
+	
+	inline void SET_CS()
+	{
+		digitalWrite(cs_pin,HIGH);	
+	}
+	
+	inline uint8_t check_message()
+	{
+		return !digitalRead(intr_pin);
+	}
+	
+	inline void bit_modify(uint8_t adress, uint8_t mask, uint8_t data)
+	{ 
+	  
+	  RESET_CS();
+	  
+	  SPI.transfer(SPI_BIT_MODIFY);
+	  SPI.transfer(adress);
+	  SPI.transfer(mask);
+	  SPI.transfer(data);
+	  
+	  SET_CS();
+	}
+
+	inline void write_register( uint8_t adress, uint8_t data)
+	{
+	  
+	  RESET_CS();
+	  
+	  SPI.transfer(SPI_WRITE);
+	  SPI.transfer(adress);
+	  SPI.transfer(data);
+	  
+	  SET_CS();
+	}
+	
+	inline void write_register( uint8_t adress, uint8_t *data, uint8_t n)
+	{
+	  
+	  RESET_CS();
+	  
+	  SPI.transfer(SPI_WRITE);
+	  SPI.transfer(adress);
+	  for(uint8_t i=0;i<n;++i){
+		SPI.transfer(*(data+i));
+	  }
+	  
+	  SET_CS();
+	}
+	
+	inline uint8_t read_register(uint8_t adress)
+	{
+	  uint8_t data;
+	  
+	  RESET_CS();
+	  
+	  SPI.transfer(SPI_READ);
+	  SPI.transfer(adress);
+	  
+	  data = SPI.transfer(0xff);  
+	  
+	  SET_CS();
+	  
+	  return data;
+	}
+
+	inline uint8_t read_status(uint8_t type)
+	{
+	  uint8_t data;
+	  
+	  RESET_CS();
+	  
+	  SPI.transfer(type);
+	  data = SPI.transfer(0xff);
+	  
+	  SET_CS();
+	  
+	  return data;
+	}
+
+	//returns Tx buffer status and message Tx error status
+	inline uint8_t Tx_buffer_Status()
+	{
+	  /*Bits defined  in mcp2515_defs.h for TXBnCTRL (n = 0, 1, 2)
+		ABTF	Message Aborted Flag bit (1: Err, 0: OK)
+		MLOA	Message Lost Arbitration bit (1: Err, 0: OK)
+		TXERR	Transmission Error Detected bit (1: Err, 0: OK)
+		TXREQ	Message Transmit Request bit (1: pending Tx, 0: Tx complete)
+		TXP1	Transmit Buffer Priority bits[1:0]
+		TXP0	
+	  */
+	  
+	  uint8_t r_status = read_status(TXB0CTRL);
+	  
+	  r_status |= read_status(TXB1CTRL);
+	  r_status |= read_status(TXB2CTRL);
+	  
+	  return r_status;
+	}
+	
+	MCP2515(uint8_t cs,uint8_t intr)
+	{
+		cs_pin = cs;
+		intr_pin = intr;
+	}
+	
+	~MCP2515(){
+		#ifdef SPI_Init
+			#undef SPI_Init
+		#endif
+		SPI.end();
+	}
+
+private:
+	uint8_t cs_pin;
+	uint8_t intr_pin;
+	uint8_t mode_chip;
+
+};
+
+#endif
